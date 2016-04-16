23a65a29d7725229833e69bbc713d0d01be46d33
vanih <vanih@student.chalmers.se>
2016-04-16 22:51:59 +0200

    Count increased, with new frequency LUT
    Count increased upto 10010 so that max multiplication gives 1111110.
    Frequency LUT is updated.

--- VHDL/LFO_Top.vhd
+++ VHDL/LFO_Top.vhd
@@ -11,15 +11,15 @@ entity LFO is
        rate_out : out std_logic;   
        LFO_gain_main_in: in std_logic_vector(6 downto 0);
        LFO_freq_main_in: in std_logic_vector(6 downto 0);
-       lfo_out:out std_logic_vector(6 downto 0));
+       lfo_out:out std_logic_vector(7 downto 0));
 end LFO;
 
 
 architecture beh of LFO is
 
-  signal factor:std_logic_vector(5 downto 0):="000001";
+  
   signal clk_divide: std_logic;
-  signal ratex: std_logic_vector(14 downto 0):="000000000000000";
+  signal ratex: std_logic_vector(13 downto 0):="00000000000000";
   signal gain: std_logic_vector(2 downto 0);
 
 
@@ -27,14 +27,14 @@ architecture beh of LFO is
 component LFO_clk is
        Port(clk_in:in std_logic;
             sample_clk:in std_logic;
-            rate_div : in std_logic_vector(14 downto 0);
+            rate_div : in std_logic_vector(13 downto 0);
             clk_out :out std_logic);
     end component;
     
 
 component LFO_freq is
           port (LFO_freq_in : in std_logic_vector(6 downto 0);
-                rate : out std_logic_vector(14 downto 0));
+                rate : out std_logic_vector(13 downto 0));
      end component; 
      
 component LFO_gain is
@@ -66,15 +66,15 @@ component LFO_gain is
 Process(clk)
   
  	 variable flag0: std_logic :='0';
-  	 variable f: std_logic_vector(3 downto 0):="0001";
-  	 variable count: std_logic_vector(3 downto 0):="0000";
-       variable count1: std_logic_vector(3 downto 0):="0000";
+  	 variable f: std_logic_vector(4 downto 0):="00001";
+  	 variable count: std_logic_vector(4 downto 0):="00000";
+       variable count1: std_logic_vector(4 downto 0):="00000";
   
   begin
     IF rising_edge(clk) THEN -- send start condition
       IF sample_clk = '1' THEN
        IF clk_divide = '1' THEN
-        if(count>="0111" or count<="0000" ) then
+        if(count>="10010" or count<="00000" ) then
         	flag0:=not flag0;
     	   end if;
         if (flag0='1') then
--- VHDL/LFO_clk.vhd
+++ VHDL/LFO_clk.vhd
@@ -7,7 +7,7 @@ USE IEEE.NUMERIC_STD.ALL;
 entity LFO_clk  is
     Port(clk_in:in std_logic;
          sample_clk:in std_logic;
-         rate_div : in std_logic_vector(14 downto 0);
+         rate_div : in std_logic_vector(13 downto 0);
          clk_out :out std_logic);
     end LFO_clk ;
  
@@ -25,16 +25,17 @@ Begin
   
   Process(clk_in)
 
-variable clockcount : std_logic_vector(14 downto 0):    ="000000000000000";
+variable clockcount : std_logic_vector(13 downto 0)
+:="00000000000000";
 variable x : std_logic:= '0';
-variable m : std_logic_vector(14 downto 0) := "000000000000001";
+variable m : std_logic_vector(13 downto 0) := "00000000000001";
   
    begin
     IF rising_edge(clk_in) THEN
        IF sample_clk = '1' THEN
         if (clockcount = rate_div-1) then
           clk_out <= '1';
-          clockcount := "000000000000000";
+          clockcount := "00000000000000";
         else
           clockcount     :=std_logic_vector(unsigned(clockcount)+unsigned(m));
           clk_out <= '0';
